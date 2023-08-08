eacc9b995c8775774f69d73aa28a18d0dd4e50c4
Saad <rs.mailpro@gmail.com>
2023-08-08 14:11:00 +0200

    2ème partie

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-web';
-import {SafeAreaProvider} from 'react-native-safe-area-context'
-import { s } from './App.style';
 import { useState } from 'react';
-import CardTodo from './components/CardTodo/CardTodo';
+import { View, SafeAreaView, ScrollView, Alert, Modal, TextInput, Button, Text, TouchableOpacity } from 'react-native';
+import { s } from './App.style';
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 import Header from './components/Header/Header';
+import CardTodo from './components/CardTodo/CardTodo';
 import TabBottomMenu from './components/TabBottomMenu/TabBottomMenu';
+// import Dialog from "react-native-dialog"
+import ButtonAdd from './components/ButtonAdd/ButtonAdd';
+import { v4 as uuidv4 } from 'uuid';
+import 'react-native-get-random-values';
 
 export default function App() {
 
   const [selectedTabName, setSelectedTabName] = useState("all")
-
-  const [todoList, setTodoList] = useState([
+  const [isAddDialogVisible, setIsAddDialogVisible] = useState(false)
+  const [inputValue, setInputValue] = useState("")
+  
+    const [todoList, setTodoList] = useState([
     {id: 1, title: "Sortir le chien", isCompleted: true},
     {id: 2, title: "Aller chez le garagisteeeeeee", isCompleted: false},
     {id: 3, title: "Faire les courses", isCompleted: false},
@@ -19,15 +24,30 @@ export default function App() {
     {id: 5, title: "Sortir le chien", isCompleted: true},
     {id: 6, title: "Aller chez le garagiste", isCompleted: false},
     {id: 7, title: "Faire les courses", isCompleted: false},
-    {id: 8, title: "Appeler le vétérinaire", isCompleted: false}
+    {id: 8, title: "Appeler le vétérinaire", isCompleted: false},
   ])
 
+  function deleteTodo(todoToDelete){
+    Alert.alert('Suppression', 'Supprimer cette tâche ?', [
+      {
+        text: 'Supprimer',
+        style: 'desctructive',
+        onPress: ()=>{
+          setTodoList(todoList.filter(todo => todo.id !== todoToDelete.id))
+        }
+      },
+      {
+        text: 'Annuler',
+        style: 'cancel'
+      }
+    ])
+  }
+
   function renderTodoList(){
-    return getFilteredList().map((todo, i) => (
-        <View style={s.cardItem}>
-          <CardTodo onPress={() => updateTodo(todo)} todo={todo}/>
-        </View>)
-    )
+    return getFilteredList().map((todo, i) => 
+    <View style={s.cardItem} key={i}>
+      <CardTodo onLongPress={deleteTodo} onPress={()=> updateTodo(todo)} todo={todo}/>
+    </View>) 
   }
 
   function getFilteredList(){
@@ -54,7 +74,23 @@ export default function App() {
     todoListCopy[indexToUpdate] = updateTodo
     setTodoList(todoListCopy)
   }
-  
+
+  function showAddDialog(){
+    setIsAddDialogVisible(true)
+  }
+
+  function addTodo(){
+    const newTodo = {
+      id: uuidv4(),
+      title: inputValue,
+      isCompleted: false,
+    }
+
+    setTodoList([...todoList, newTodo])
+    setIsAddDialogVisible(false)
+    setInputValue('')
+  }
+
   return (
     <>
       <SafeAreaProvider>
@@ -64,22 +100,36 @@ export default function App() {
             {renderTodoList()}
           </ScrollView>
           <View style={s.footer}>
-            <TabBottomMenu 
-              todoList={todoList} 
-              onPress={setSelectedTabName} 
-              selectedTabName={selectedTabName}/>
           </View>
-        </SafeAreaView>
+          <ButtonAdd onPress={showAddDialog} />
+          <TabBottomMenu 
+        todoList={todoList} 
+        onPress={setSelectedTabName} 
+        selectedTabName={selectedTabName}/>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Modal
+            transparent={true}
+            visible={isAddDialogVisible}
+            onRequestClose={()=> setIsAddDialogVisible(false)}>
+            <TouchableOpacity
+              style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}
+              activeOpacity={1}
+              onPress={()=> setIsAddDialogVisible(false)}>
+              <View onStartShouldSetResponder={() => true} style={{ width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10, elevation: 5 }}>
+                <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Créer une tâche</Text>
+                <Text style={{ marginVertical: 10 }}>Choisi un nom pour la nouvelle tâche</Text>
+                <TextInput
+                  style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+                  onChangeText={setInputValue}
+                  value={inputValue}
+                />
+                <Button title="Créer" onPress={addTodo} disabled={inputValue.trim().length === 0} />
+              </View>
+            </TouchableOpacity>
+          </Modal>
+        </View>
+        </SafeAreaView>   
       </SafeAreaProvider>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/CardTodo/CardTodo.js
+++ components/CardTodo/CardTodo.js
@@ -2,16 +2,14 @@ import {TouchableOpacity, Image, Text} from 'react-native'
 import checkImg from '../../assets/check.png'
 import { s } from './CardTodo.style'
 
-export default function CardTodo({todo, onPress}) {
-    return (
-        <TouchableOpacity style={s.card} onPress={() => {onPress()}}>
-            <Text style={[
-                s.txt,
-                todo.isCompleted && {textDecorationLine: "line-through"}
-            ]}>
-                {todo.title}
-            </Text>
-            {todo.isCompleted && <Image style={s.img} source={checkImg}/>}
-        </TouchableOpacity>
-    )
+export default function CardTodo({todo, onPress, onLongPress}) {
+  return (
+    <TouchableOpacity style={s.card} onPress={()=> onPress()} onLongPress={()=>onLongPress(todo)}>
+        <Text style={[
+            s.txt, 
+            todo.isCompleted && {textDecorationLine: "line-through"}
+        ]}>{todo.title}</Text>
+        {todo.isCompleted && <Image style={s.img} source={checkImg} />}
+    </TouchableOpacity>
+  )
 }
